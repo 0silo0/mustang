540cf6c780055bfbd421bcc9ed2626ee9c2ad30b
0silo0 <nikita.vershinin2003@mail.ru>
2025-08-22 23:38:26 +0500

    Правки конфигурации

---
+++ mustang59-website/next.config.js
@@ -0,0 +1,16 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'standalone',
+  trailingSlash: false,
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/',
+        permanent: false,
+      },
+    ];
+  }
+}
+
+module.exports = nextConfig
