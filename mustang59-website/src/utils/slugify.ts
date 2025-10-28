f0e6e195f626c6d09576e314849621c88ba85a36
0silo0 <nikita.vershinin2003@mail.ru>
2025-10-28 21:20:54 +0500

    Удаление мусора

--- mustang59-website/src/utils/slugify.ts
+++
@@ -1,9 +0,0 @@
-// src/utils/slugify.ts
-export function createSlug(text: string): string {
-  return text
-    .toLowerCase()
-    .replace(/\s+/g, '-')
-    .replace(/[^a-z0-9-]/g, '')
-    .replace(/-+/g, '-')
-    .trim();
-}
